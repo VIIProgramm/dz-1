2e6cba1ac3eb71547d2b8d7616e91a4119681bde
VIIProgramm <vlad1.1gordeev@gmail.com>
2023-06-29 21:17:26 +0300

    dz-1

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import './style.css'
+
+// Задание 1
+let name = prompt('Введите имя')
+alert(name)
+
+// Задание 2
+const year = 2023
+let yearOfBirth = +(prompt('Введите год важего рождения') as string)
+alert(year - yearOfBirth)
+
+// Задание 3
+let lenght = +(prompt('Введите длинну квадрата') as string)
+alert(lenght ** 3)
+
+// Задание 4
+let radius = +(prompt('Введите радиус окружности') as string)
+alert((radius ** 2) * Math.PI)
+
+// Задание 5
